1715d357969a3b774afb9004d4a840a3e4589342
Diego Patricio Durante <diegopdurante@gmail.com>
2020-07-17 04:28:37 -0300

    Added script to run with mobilenet converted from tensorflow

---
+++ run_w_mobilenet_tensorflow.sh
@@ -0,0 +1,1 @@
+python main.py -i resources/Pedestrian_Detect_2_1_1.mp4 -m raw-models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.xml -d CPU -pt 0.65 --wait_ms 1 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://0.0.0.0:3004/fac.ffm 
